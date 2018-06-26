4d8f4a98f406cb25840a9f22125fe825367c490c
Jonathan Lui <jonathanlui@google.com>
2018-06-26 15:51:18 -0700

    synth: check in synth tool

---
+++ synth.py
@@ -0,0 +1,32 @@
+import synthtool as s
+import synthtool.gcp as gcp
+import logging
+from pathlib import Path
+import subprocess
+
+logging.basicConfig(level=logging.DEBUG)
+
+gapic = gcp.GAPICGenerator()
+
+versions = ['v1beta']
+
+for version in versions:
+    library = gapic.node_library(
+        'os-login', version,
+        config_path="/google/cloud/oslogin/"
+                    f"artman_oslogin_{version}.yaml")
+
+    # skip index, protos, package.json, and README.md
+    s.copy(
+        library,
+        excludes=['package.json', 'README.md', 'src/index.js'],
+    )
+
+#
+# Node.js specific cleanup
+#
+subprocess.run(['npm', 'install'])
+
+# # prettify and lint
+subprocess.run(['npm', 'run', 'prettier'])
+subprocess.run(['npm', 'run', 'lint'])
