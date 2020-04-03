c2907acf9a059a18763d7bc2c1a2da083b6567ef
yoshi-automation <yoshi-automation@google.com>
2020-04-03 04:58:48 -0700

    Change triggered by none of the following: This git repo (https://github.com/googleapis/nodejs-os-login.git)

--- synth.py
+++ synth.py
@@ -21,9 +21,6 @@ import subprocess
 
 logging.basicConfig(level=logging.DEBUG)
 
-AUTOSYNTH_MULTIPLE_COMMITS = True
-
-
 gapic = gcp.GAPICMicrogenerator()
 versions = ['v1beta', 'v1']
 for version in versions:
